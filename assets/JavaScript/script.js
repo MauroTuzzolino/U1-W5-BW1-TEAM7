0e1d46e141d2ec6480fc1ae8f08659a442f86aeb
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-04-02 17:51:18 +0200

    sistemazione css del form di inizio test

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,84 +1,3 @@
-/*
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "What does CPU stand for?",
-    correct_answer: "Central Processing Unit",
-    incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
-    correct_answer: "Final",
-    incorrect_answers: ["Static", "Private", "Public"],
-  },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question: "The logo for Snapchat is a Bell.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
-  },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question: "Pointers were not used in the original C programming language; they were added later on in C++.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "What is the most preferred image format used for logos in the Wikimedia database?",
-    correct_answer: ".svg",
-    incorrect_answers: [".png", ".jpeg", ".gif"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "In web design, what does CSS stand for?",
-    correct_answer: "Cascading Style Sheet",
-    incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "What is the code name for the mobile operating system Android 7.0?",
-    correct_answer: "Nougat",
-    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "On Twitter, what is the character limit for a Tweet?",
-    correct_answer: "140",
-    incorrect_answers: ["120", "160", "100"],
-  },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question: "Linux was first created as an alternative to Windows XP.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "Which programming language shares its name with an island in Indonesia?",
-    correct_answer: "Java",
-    incorrect_answers: ["Python", "C", "Jakarta"],
-  },*/
 let questions;
 const fetchQ = async () => {
   try {
@@ -95,6 +14,10 @@ const fetchQ = async () => {
 const questionsFullFill = function (data) {
   questions = data;
   console.log(questions);
+  const time = document.getElementById("benchmarkTimer");
+  time.style.display = "block";
+  const footer = document.getElementsByTagName("footer")[0];
+  footer.style.display = "block";
   cycleFunction();
   testQuestionSkipper();
   timerFunction();
@@ -160,7 +83,7 @@ const clicked = function () {
 
 const testQuestionSkipper = function () {
   question.innerText = questions[i].question;
-  questionCounter.innerText = `QUESTION ${i + 1} / 10`;
+  questionCounter.innerText = `QUESTION ${i + 1} / ${questions.length}`;
   containerOfQuestionAnswerContainer.innerHTML = "";
   const rAP = randomArrayPosition(allAnswer[i]);
   const questionAnswerContainer = document.createElement("div");
@@ -197,6 +120,7 @@ const verifyAnswer = function () {
     if (choosenAnswer.innerText === questions[i].correct_answer) {
       correctAnswerCounter++;
       localStorage.setItem("sharedData", correctAnswerCounter);
+      localStorage.setItem("questionsLenght", questions.lenght);
     }
   }
 };
@@ -220,7 +144,7 @@ nextQuestionButton.addEventListener("click", function () {
   counter = 60;
   //console.log(correctAnswerCounter);
   i++;
-  if (i === 10) {
+  if (i === questions.length) {
     window.location.href = "resultsPage.html";
   }
   setTimeout(() => {
